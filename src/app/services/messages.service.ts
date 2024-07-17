456bc01230a0f9bc8c89fabe9fce375d92dd7794
moremig29 <miguelangel.moreira@gmail.com>
2024-07-17 15:45:09 -0400

    env and ruta api

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,15 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Message } from '../interfaces/message.interface';
-
+import { Message } from '../interfaces/Message.interface';
+import { environment } from '../../environments/environment';
+const env = environment;
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
-  apikey: string =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJpNHNzbTh6a3h5YjI2b3pjM3N3bWszampmb2M2ZHdwZWowbndqeXg0czFoa211dmcwYyIsInNlY3JldCI6IjZkOTM0a200em42Mnp3ZWhrZmwiLCJpYXQiOjE3MjEyNDA5NjZ9.tm5xpa6q01Un7GCOleFEfhijtZY4_6znv5UStq4jeiI';
-  baseUrl: string = 'http://localhost:4000/api';
-  //baseUrl: string = 'https://deuxserver.onrender.com/api',
+  apikey = env.apikey
+  baseUrl = env.baseUrl
 
   get headers() {
     return {
