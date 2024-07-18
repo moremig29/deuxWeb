3333e4fef33c7778055da04714eaa6e4fbbb2937
moremig29 <miguelangel.moreira@gmail.com>
2024-07-18 13:55:37 -0400

    Ignore unwanted files

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  apikey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJpNHNzbTh6a3h5YjI2b3pjM3N3bWszampmb2M2ZHdwZWowbndqeXg0czFoa211dmcwYyIsInNlY3JldCI6IjZkOTM0a200em42Mnp3ZWhrZmwiLCJpYXQiOjE3MjEyNDA5NjZ9.tm5xpa6q01Un7GCOleFEfhijtZY4_6znv5UStq4jeiI',
-  //baseUrl: 'http://localhost:4000/api',
-  baseUrl: 'https://deuxserver.onrender.com/api',
-};
